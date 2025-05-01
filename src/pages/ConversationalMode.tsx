5bf192c24ad1a1f5e5343ce6483f4c980a6c828e
AKASHDHARDUBEY <akashdhar2112@gmail.com>
2025-05-01 17:26:36 +0530

    _

--- src/pages/ConversationalMode.tsx
+++ src/pages/ConversationalMode.tsx
@@ -73,7 +73,6 @@ const ConversationalMode = () => {
 
     // Find the best matching keyword
     let bestMatch = null;
-    let bestMatchKey ;
     let maxMatches = 0;
 
     for (const [keyword, entryKey] of Object.entries(keywordMap)) {
